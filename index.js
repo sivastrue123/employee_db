1ae4017dd5b0f17fb1dc57dd4f72abad2c6240a7
siva <sivav2535@gmail.com>
2025-08-13 11:00:45 +0530

    Remove testing controller and model

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-import 'dotenv/config'; 
-import express from 'express';
-import connectDB from './utils/DbConnector.js'
-import userRoutes from './router/userRouter.js'; 
-import employeeRoutes from "./router/employeeRouter.js"
+import "dotenv/config";
+import express from "express";
+import connectDB from "./utils/DbConnector.js";
+
+import employeeRoutes from "./router/employeeRouter.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
-
 
 connectDB();
 
-
-app.use('/api/users', userRoutes);
-app.use('/api/employee',employeeRoutes)
-
-
-
-
+app.use("/api/employee", employeeRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`Access the User API at http://localhost:${PORT}/api/users`);
-     console.log(`Access the Employee API at http://localhost:${PORT}/api/employee`);
+  console.log(`Server running on port ${PORT}`);
+
+  console.log(
+    `Access the Employee API at http://localhost:${PORT}/api/employee`
+  );
 });
--- router/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'; 
-import { createUser, getAllUsers } from '../controller/user.controller.js'; 
-const router = express.Router();
-
-
-router.post('/', createUser);
-router.get('/', getAllUsers);
-
-export default router;
