395235aa1ad490330f8c982a146ce0bcb1eae6d9
siva <sivav2535@gmail.com>
2025-09-17 17:37:35 +0530

    add some variable for testing in web push

--- router/webPush.js
+++ router/webPush.js
@@ -1,5 +1,5 @@
 // router/webPush.js
-import express, { response } from "express";
+import express from "express";
 import webpush from "web-push";
 import PushSubscription from "../model/pushNotification.model.js";
 import Employee from "../model/employee.model.js"; // ⚠️ adjust path if needed
@@ -149,8 +149,9 @@ router.post("/clockin", async (req, res) => {
       }
     } else {
       for (const sub of subs) {
+        sent=5
         try {
-        const response=  await webpush.sendNotification(
+          await webpush.sendNotification(
             { endpoint: sub.endpoint, keys: sub.keys },
             payload,
             {
@@ -159,8 +160,7 @@ router.post("/clockin", async (req, res) => {
               topic: "clockin", // collapse key
             }
           );
-          console.log(response)
-          sent=sent+1;
+          ;
         } catch (e) {
           if (e.statusCode === 404 || e.statusCode === 410) {
             await removeSubscriptionByEndpoint(sub.endpoint);
@@ -175,7 +175,7 @@ router.post("/clockin", async (req, res) => {
       }
     }
 
-    return res.json({ ok: true, count: sent, subs: subs, length: subs.length,res:response });
+    return res.json({ ok: true, count: sent, subs: subs, length: subs.length });
   } catch (e) {
     console.error("clockin route failed:", e);
     return res
